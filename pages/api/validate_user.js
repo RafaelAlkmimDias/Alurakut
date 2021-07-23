4f7c64c0f935d461b8682019729772c529726750
RafaelAlkmimDias <rafas007@hotmail.com>
2021-07-23 16:52:45 -0300

    correção

--- pages/api/validate_user.js
+++ pages/api/validate_user.js
@@ -2,11 +2,13 @@ import { getUserExists } from '../../src/services/Communication';
 import jwt from 'jsonwebtoken';
 
 export default async function validate_user(request,response){
-    if(!request.headers.authorization){
+    const token = request.headers.authorization
+    const haveAuthorization = Boolean(token)
+    if(haveAuthorization){
         const decodedInfo = jwt.decode(token)
         const user = decodedInfo.githubUser;
         const dado = await getUserExists(user);
-    
+
         response.json({ exists: dado });
         return;
 
